0886904b717786251a0aba2ba1f4f8a12b50e815
Danilo Alexandrino <danilo.alexandrinodm@gmail.com>
2020-01-19 21:01:45 -0300

    Ajuste no sensor

--- sketch_jan11a.ino
+++ sketch_jan11a.ino
@@ -12,6 +12,7 @@ uint8_t led1 = D4;
 uint8_t led2 = D3;
 int LDRPin = A0; 
 int sensorValue=0;
+int varAux=0;
 
 void setup()
 {
@@ -40,15 +41,17 @@ void setup()
 void loop()
 {
 
-  if (sensorValue < 100)
+  if (sensorValue < 20 and varAux == 0)
   {
     digitalWrite(led2, LOW);
-    Serial.println("LED 2 ligado, luminosidade do ambiente estava baixa!");
+    Serial.println("Led vermelho ligado, luminosidade do ambiente estava baixa!");
+    varAux = 1;
   }
-  else
+  else if(sensorValue > 20 and varAux == 1)
   {
     digitalWrite(led2, HIGH);
-    Serial.println("LED 2 desligado, luminosidade do ambiente estava alta!");
+    Serial.println("Led vermelho desligado, luminosidade do ambiente estava alta!");
+    varAux = 0;
   }
   
   sensorValue=analogRead(LDRPin);   //read  the value of the photoresistor.
@@ -56,22 +59,21 @@ void loop()
   
   //Variavel que armazena a mensagem recebida
   TBMessage msg;
-
   //Verifica se chegou alguma mensagem
   if (myBot.getNewMessage(msg))
   {
     //Verifica se foi recebia a mensagem "Liga 1"
-    if (msg.text.equalsIgnoreCase("LIGA 1"))
+    if (msg.text.equalsIgnoreCase("LIGAR LED VERDE"))
     {
       //Liga o led 1 (vermelho)
       digitalWrite(led1, LOW);
       //Retorna uma pensagem informando que o led foi aceso
-      myBot.sendMessage(msg.sender.id, "LED 1 Aceso!");     
+      myBot.sendMessage(msg.sender.id, "Led verde aceso!");     
     }
-    else if (msg.text.equalsIgnoreCase("DESLIGA 1"))
+    else if (msg.text.equalsIgnoreCase("DESLIGAR LED VERDE"))
     {
       digitalWrite(led1, HIGH);
-      myBot.sendMessage(msg.sender.id, "LED 1 Apagado!");
+      myBot.sendMessage(msg.sender.id, "Led verde Apagado!");
     }
 //    else if (msg.text.equalsIgnoreCase("LIGA 2"))
 //    {
@@ -83,22 +85,22 @@ void loop()
 //      digitalWrite(led2, HIGH);
 //      myBot.sendMessage(msg.sender.id, "LED 2 Apagado!");
 //    }
-    else if (sensorValue < 100 and msg.text.equalsIgnoreCase("Luminosidade"))
+    else if (sensorValue < 20 and msg.text.equalsIgnoreCase("Luminosidade"))
     {
       digitalWrite(led2, LOW);
-      myBot.sendMessage(msg.sender.id, "LED 2 ligado, luminosidade do ambiente estava baixa!");
+      myBot.sendMessage(msg.sender.id, (String)"Led vermelho ligado, luminosidade do ambiente esta em "+sensorValue+(String)", considerada baixa!");
     }
-    else if(sensorValue > 100 and msg.text.equalsIgnoreCase("Luminosidade"))
+    else if(sensorValue > 20 and msg.text.equalsIgnoreCase("Luminosidade"))
     {
       digitalWrite(led2, HIGH);
-      myBot.sendMessage(msg.sender.id, "LED 2 desligado, luminosidade do ambiente estava alta!");
+      myBot.sendMessage(msg.sender.id, (String)"Led vermelho desligado, luminosidade do ambiente está em "+sensorValue+(String)", considerada alta!");
     }
     else
     {
       //Caso receba qualquer outro comando, envia uma
       //mensagem generica/informativa
       String reply;
-      reply = "Desculpe, não entendi. \n\nSegue a lista de comandos que consigo entender: \nLiga 1- Para ligar led 1 \nDesliga 1 - Para desligar led 1 \nLuminosidade - Para checar luminosidade do ambiente e acender ou não o led 2.";
+      reply = "Desculpe, não entendi. \n\nSegue a lista de comandos que consigo entender: \n\nLigar led verde - Para ligar led verde \n\nDesligar led verde - Para desligar led verde \n\nLuminosidade - Para checar luminosidade do ambiente e acender ou não o led vermelho.";
       myBot.sendMessage(msg.sender.id, reply);
     }
   }
